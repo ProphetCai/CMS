b43283b2fcc0f6e5d7486d9f81ba799c2555185b
Prophet <770114081@qq.com>
2019-12-27 15:20:30 +0800

    test

---
+++ sql/cms.sql
@@ -0,0 +1,66 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : briup
+Source Server Version : 50529
+Source Host           : localhost:3306
+Source Database       : cms
+
+Target Server Type    : MYSQL
+Target Server Version : 50529
+File Encoding         : 65001
+
+Date: 2019-12-27 15:16:37
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for cms_article
+-- ----------------------------
+DROP TABLE IF EXISTS `cms_article`;
+CREATE TABLE `cms_article` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `author` varchar(255) DEFAULT NULL,
+  `content` text,
+  `title` varchar(255) DEFAULT NULL,
+  `click_times` int(11) NOT NULL,
+  `publish_date` datetime DEFAULT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK5jaj0e90ajnv2341hjqvjtole` (`category_id`),
+  CONSTRAINT `FK5jaj0e90ajnv2341hjqvjtole` FOREIGN KEY (`category_id`) REFERENCES `cms_category` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for cms_category
+-- ----------------------------
+DROP TABLE IF EXISTS `cms_category`;
+CREATE TABLE `cms_category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `code` bigint(20) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for cms_customer
+-- ----------------------------
+DROP TABLE IF EXISTS `cms_customer`;
+CREATE TABLE `cms_customer` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for cms_link
+-- ----------------------------
+DROP TABLE IF EXISTS `cms_link`;
+CREATE TABLE `cms_link` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
